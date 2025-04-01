6ee597861ebd4d1535ad77f1a9d4f86efd314a44
artem <qvikhoney@gmail.com>
2025-04-01 12:45:15 +0300

    small syntax fix 2

--- api/send-message.js
+++ api/send-message.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
     const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
     const CHAT_ID = process.env.CHAT_ID;
 
-    const url = `https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=У тебе замовлення від Аделіни)`;
+    const url = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=У тебе замовлення від Аделіни)`;
 
     try {
         const response = await fetch(url);
